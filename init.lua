eb0b3bbe0ce412943b74c62fc451e1d24ecad5af
vohonen <vili.kohonen@aalto.fi>
2025-04-16 08:42:20 +0300

    keymaps last to override plugin keymaps

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
-require('options') 			-- neovim options
-require('keymaps')			-- additional keymaps
-require('config/lazy')		-- lazy plugin manager config
-require('send_code') 		-- send code to a terminal pane
-
 -- plugins
+require('config/lazy')		-- lazy plugin manager config
 require('plugins/autopairs')
 require('plugins/colorschemes')
 require('plugins/indent')
@@ -12,3 +8,8 @@ require('plugins/nvim-cmp')
 require('plugins/nvim-tree')
 require('plugins/nvim-treesitter')
 require('plugins/vimtex')
+
+require('options') 			-- neovim options
+require('keymaps')			-- additional keymaps
+require('send_code') 		-- send code to a terminal pane
+
