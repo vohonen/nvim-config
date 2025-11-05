a4379363f621c6ae04d20ce63c9c94349783a97b
vohonen <vili.kohonen@aalto.fi>
2025-11-05 10:35:40 +0200

    remove duplicate code

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,6 @@
 vim.keymap.set('n', '<leader>mp', ':MarkdownPreview<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ms', ':MarkdownPreviewStop<CR>', { noremap = true, silent = true })
 
--- Open preview in a new Firefox window
--- vim.g.mkdp_browser = 'firefox --new-window'
-vim.g.mkdp_browser = 'firefox -P default --new-instance'
-
 -- Auto-start preview when opening markdown files
 vim.g.mkdp_auto_start = 0
 
