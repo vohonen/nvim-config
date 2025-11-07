e1ea58ab173910114e062ebea4b336c313579dd1
vohonen <vili.kohonen@aalto.fi>
2025-11-07 10:29:22 +0200

    system register copy-paste but includes randomly more stuff

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,81 +1,89 @@
 -- remap space as leader key
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- toggle search highlighting
-vim.api.nvim_set_keymap('n', '<leader>h', ':set hlsearch!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>h", ":set hlsearch!<CR>", { noremap = true, silent = true })
 
 -- copy, cut and paste with system register
-vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<C-x>', '"+d', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-v>', '"+P', { noremap = false, silent = true })
+-- vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('v', '<C-x>', '"+d', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<C-v>', '"+P', { noremap = false, silent = true })
+-- add shift to include visual blocks with <C-v>
+-- change to y (yank) and p (paste) to work better with alacritty
+vim.api.nvim_set_keymap("v", "<C-S-y>", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-S-x>", '"+d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-S-p>", '"+P', { noremap = false, silent = true })
 
--- close vertically split buffers without removing the split 
-vim.api.nvim_set_keymap('n', '<leader>bd', ':bp<bar>vsp<bar>bn<bar>bd<CR>', { noremap = true, silent = true })
+-- close vertically split buffers without removing the split
+vim.api.nvim_set_keymap("n", "<leader>bd", ":bp<bar>vsp<bar>bn<bar>bd<CR>", { noremap = true, silent = true })
 
 -- go to the end of the file and move line to the center of the screen
 -- workaround because the scrolloff option isn't working
-vim.api.nvim_set_keymap('n', '<leader><S-g>', '<S-g>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><S-g>", "<S-g>zz", { noremap = true, silent = true })
 
 -- better window movement
-vim.api.nvim_set_keymap('n', '<S-h>', '<C-w>h', { silent = true })
-vim.api.nvim_set_keymap('n', '<S-j>', '<C-w>j', { silent = true })
-vim.api.nvim_set_keymap('n', '<S-k>', '<C-w>k', { silent = true })
-vim.api.nvim_set_keymap('n', '<S-l>', '<C-w>l', { silent = true })
+vim.api.nvim_set_keymap("n", "<S-h>", "<C-w>h", { silent = true })
+vim.api.nvim_set_keymap("n", "<S-j>", "<C-w>j", { silent = true })
+vim.api.nvim_set_keymap("n", "<S-k>", "<C-w>k", { silent = true })
+vim.api.nvim_set_keymap("n", "<S-l>", "<C-w>l", { silent = true })
 
 -- insert and append one character in normal mode
 local function insert_char(mode)
-  local char = vim.fn.getchar()
-  if char == 27 then -- ESC key
-    return
-  end
-  local char_str = vim.fn.nr2char(char)
-  if mode == 'insert' then
-    vim.cmd('normal! i' .. char_str .. '\27') -- \27 is ESC
-  else -- append
-    vim.cmd('normal! a' .. char_str .. '\27')
-  end
+	local char = vim.fn.getchar()
+	if char == 27 then -- ESC key
+		return
+	end
+	local char_str = vim.fn.nr2char(char)
+	if mode == "insert" then
+		vim.cmd("normal! i" .. char_str .. "\27") -- \27 is ESC
+	else -- append
+		vim.cmd("normal! a" .. char_str .. "\27")
+	end
 end
 
-vim.keymap.set('n', ',i', function() insert_char('insert') end, { desc = 'Insert one character' })
-vim.keymap.set('n', ',a', function() insert_char('append') end, { desc = 'Append one character' })
+vim.keymap.set("n", ",i", function()
+	insert_char("insert")
+end, { desc = "Insert one character" })
+vim.keymap.set("n", ",a", function()
+	insert_char("append")
+end, { desc = "Append one character" })
 
 -- tab switch buffers
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
 -- tab overrides <C-i> jump list behaviour, map it to <C-p>
-vim.keymap.set('n', '<C-p>', '<C-i>', { noremap = true })
+vim.keymap.set("n", "<C-p>", "<C-i>", { noremap = true })
 
 -- insert a new line with Enter from normal mode
-vim.api.nvim_set_keymap('n', '<CR>', 'o<Esc>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><CR>', 'O<Esc>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<CR>", "o<Esc>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><CR>", "O<Esc>j", { noremap = true, silent = true })
 
 -- save and quit with leader
-vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>wq', ':wq<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>wa', ':wqa<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>qq', ':q!<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>qa', ':qa!<CR>', {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>wq", ":wq<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>wa", ":wqa<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>qq", ":q!<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>qa", ":qa!<CR>", { noremap = true })
 
 -- nvim-tree toggle
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true })
 
 -- move down a directory
-vim.api.nvim_set_keymap('n', '<Leader>.', ':cd ..<CR>', {noremap = true})
-
+vim.api.nvim_set_keymap("n", "<Leader>.", ":cd ..<CR>", { noremap = true })
 
 -- Git mappings
-vim.api.nvim_set_keymap('n', '<Leader>gg', ':G <CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gaa', ':G add .<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gaf', ':G add %<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gd', ':G diff<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gc', ':G commit<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>go', ':G pull<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gp', ':G push<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gll', ':G log<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>glp', ':G log -p<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>gaw', ':G add .<bar> :G commit <CR>', {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>gg", ":G <CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gaa", ":G add .<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gaf", ":G add %<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gd", ":G diff<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gc", ":G commit<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>go", ":G pull<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gp", ":G push<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gll", ":G log<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>glp", ":G log -p<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gaw", ":G add .<bar> :G commit <CR>", { noremap = true })
 
 -- quote and bracket completion
 -- vim.api.nvim_set_keymap('i', '"', '""<left>', { noremap = true, silent = true })
@@ -87,20 +95,26 @@ vim.api.nvim_set_keymap('n', '<Leader>gaw', ':G add .<bar> :G commit <CR>', {nor
 -- vim.api.nvim_set_keymap('i', '{<CR>', '{<CR>}<ESC>O', { noremap = true, silent = true })
 
 -- GNU make shortcuts
-vim.api.nvim_set_keymap('n', '<leader>mm', ':!make<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ma', ':!make all<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>mc', ':!make clean<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mm", ":!make<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ma", ":!make all<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mc", ":!make clean<CR>", { noremap = true, silent = true })
 -- for PAT course in Aalto
-vim.api.nvim_set_keymap('n', '<leader>mpat',
-	':!g++ -O3 -Werror -Wall --pedantic -std=c++17 -march=native -fopenmp -o %:r % && ./%:r<CR>',
-	{ noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>mdb',
-	':!g++ -ggdb -O0 -Werror -Wall -fsanitize=address --pedantic -std=c++17 -march=native -fopenmp -o %:r.db %<CR>',
-	{ noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>mpat",
+	":!g++ -O3 -Werror -Wall --pedantic -std=c++17 -march=native -fopenmp -o %:r % && ./%:r<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>mdb",
+	":!g++ -ggdb -O0 -Werror -Wall -fsanitize=address --pedantic -std=c++17 -march=native -fopenmp -o %:r.db %<CR>",
+	{ noremap = true, silent = true }
+)
 -- for PPC course in aalto
-vim.api.nvim_set_keymap('n', '<leader>mpr', ':!./grading --remote test<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>mpb', ':!./grading --remote benchmark<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>mpp', ':!./grading test<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mpr", ":!./grading --remote test<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mpb", ":!./grading --remote benchmark<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mpp", ":!./grading test<CR>", { noremap = true, silent = true })
 
 -- shortcut to show date
-vim.api.nvim_set_keymap('n', '<leader>da', ':!date<CR>', { noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>da", ":!date<CR>", { noremap = true })
