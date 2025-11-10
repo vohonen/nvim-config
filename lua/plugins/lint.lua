f0dbe16411071d77d9ca108a08b071885d9c417a
vohonen <vili.kohonen@aalto.fi>
2025-11-10 15:45:07 +0200

    quiet chktex

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,18 +1,21 @@
-local lint = require('lint')
+local lint = require("lint")
 
 -- Configure linters by filetype
 lint.linters_by_ft = {
-  python = { 'ruff' },
-  tex = { 'chktex' },
-  lua = { 'luacheck' },
-  markdown = { 'markdownlint' },
+	python = { "ruff" },
+	tex = { "chktex" },
+	lua = { "luacheck" },
+	markdown = { "markdownlint" },
 }
 
 -- Auto-lint on these events
-local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-  group = lint_augroup,
-  callback = function()
-    lint.try_lint()
-  end,
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	group = lint_augroup,
+	callback = function()
+		lint.try_lint()
+	end,
 })
+
+-- Configure chktex to shut up
+lint.linters.chktex.ignore_exitcode = true
