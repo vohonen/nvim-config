f47f919834711b4198262378c9f83f0badcb3146
vohonen <vili.kohonen@aalto.fi>
2025-12-05 11:35:10 +0200

    linting

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,22 +1,20 @@
-
--- remove vimtex syntax highlighting 
+-- remove vimtex syntax highlighting
 -- not to overlap with treesitter
 vim.g.vimtex_syntax_enabled = 0
 
-vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_quickfix_mode = 0
-vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_compiler_latexmk = {
-    out_dir = 'build',
-    callback = 1,
-    continuous = 1,
-    options = {
-        '-pdf',
-        '-pdflatex=pdflatex -interaction=nonstopmode -synctex=1 -file-line-error',
-        '-bibtex',
-        '-verbose',
-        '-halt-on-error',
-        '-use-make'
-    }
+	out_dir = "build",
+	callback = 1,
+	continuous = 1,
+	options = {
+		"-pdf",
+		"-pdflatex=pdflatex -interaction=nonstopmode -synctex=1 -file-line-error",
+		"-bibtex",
+		"-verbose",
+		"-halt-on-error",
+		"-use-make",
+	},
 }
-
